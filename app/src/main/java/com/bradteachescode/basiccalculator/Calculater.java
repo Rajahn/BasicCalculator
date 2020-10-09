a07214968973ea9cf794b27351708a9762018516
Rajahn <39303094+Rajahn@users.noreply.github.com>
2020-10-09 21:41:47 +0800

    Initial code for passing TDD test

---
+++ app/src/main/java/com/bradteachescode/basiccalculator/Calculater.java
@@ -0,0 +1,15 @@
+package com.bradteachescode.basiccalculator;
+
+public class Calculater {
+
+
+    public String addNumber(String num,String number){
+
+        number+=num;
+
+        return number;
+    }
+
+
+
+}
